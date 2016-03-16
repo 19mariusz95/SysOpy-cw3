4419750e8324afd151510bfb0c7e333e366ad158
19mariusz95 <19mariusz95@gmail.com>
2016-03-17 00:06:24 +0100

    zad 1 all options

--- zad1/main.c
+++ zad1/main.c
@@ -1,14 +1,24 @@
+#define _GNU_SOURCE
+
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/times.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
 
 int licznik = 0;
 
-void do_stuff(int n, char option);
+int fn(void *arg) {
+    licznik++;
+    _exit(0);
+}
 
 int main(int argc, char *argv[]) {
 
+    pid_t pid;
     if (argc < 3) {
         printf("Not enough arguments");
         exit(1);
@@ -20,32 +30,67 @@ int main(int argc, char *argv[]) {
     }
     char *op = argv[2];
     char option = op[0];
-
-    clock_t time;
+    clock_t t;
     struct tms *t_cpu1 = malloc(sizeof(struct tms));
     struct tms *t_cpu2 = malloc(sizeof(struct tms));
     if (t_cpu1 == NULL || t_cpu2 == NULL) {
         printf("%s", "Allocation memory failed");
         return 1;
     }
+    t = times(t_cpu1);
+    void *child_stack = malloc(8192);
+    child_stack += 8192;
+    for (int i = 0; i < n; i++) {
+        switch (option) {
+            case 'a': {
+                pid = fork();
+                if (pid == 0) {
+                    licznik++;
+                    _exit(0);
+                } else {
+                    int status;
+                    wait(&status);
+                }
+                break;
+            }
+            case 'b': {
+                pid = vfork();
+                if (pid == 0) {
+                    licznik++;
+                    _exit(licznik);
+                } else {
+                    int status;
+                    wait(&status);
+                }
+                break;
+            }
+            case 'c': {
+                for (int j = 0; j < n; j++) {
+                    clone(&fn, child_stack, SIGCHLD, NULL);
+                    int status;
+                    wait(&status);
+                }
+                break;
+            }
+            case 'd': {
+                clone(&fn, child_stack, SIGCHLD | CLONE_VM | CLONE_VFORK, NULL);
+                int status;
+                wait(&status);
+            }
+                break;
+            default:
+                break;
+        }
+    }
 
-    time = times(t_cpu1);
-
-    do_stuff(n, option);
-
-    time = times(t_cpu2) - time;
-    printf("Real Time: %jd, User Time: %jd, System Time: %jd\n", (intmax_t) time,
+    t = times(t_cpu2) - t;
+    printf("Real Time: %jd, User Time: %jd, System Time: %jd\n", (intmax_t) t,
            (intmax_t) (t_cpu2->tms_utime - t_cpu1->tms_utime),
            (intmax_t) (t_cpu2->tms_stime - t_cpu1->tms_stime));
+    printf("licznik: %d\n", licznik);
 
-
+    free(t_cpu1);
+    free(t_cpu2);
     return 0;
 }
 
-void do_stuff(int n, char option) {
-    int i;
-    for (i = 0; i < n; i++) {
-        // TODO
-    }
-    printf("%d\n", licznik);
-}
