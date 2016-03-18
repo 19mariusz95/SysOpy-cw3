3ce4bf6e80ec7dff543caa3b3c5c9b33534a1339
19mariusz95 <19mariusz95@gmail.com>
2016-03-18 16:05:46 +0100

    zad 1 times

--- zad1/main.c
+++ zad1/main.c
@@ -7,6 +7,7 @@
 #include <sys/times.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/resource.h>
 #include <time.h>
 
 int licznik = 0;
@@ -28,69 +29,79 @@ int main(int argc, char *argv[]) {
         printf("Wrong first argument, should be a natural number");
         exit(1);
     }
+    clock_t starttime = clock();
     char *op = argv[2];
     char option = op[0];
-    clock_t t;
-    struct tms *t_cpu1 = malloc(sizeof(struct tms));
-    struct tms *t_cpu2 = malloc(sizeof(struct tms));
-    if (t_cpu1 == NULL || t_cpu2 == NULL) {
-        printf("%s", "Allocation memory failed");
-        return 1;
-    }
-    t = times(t_cpu1);
     void *child_stack = malloc(8192);
+    clock_t childtimes = 0;
     child_stack += 8192;
     for (int i = 0; i < n; i++) {
         switch (option) {
             case 'a': {
                 pid = fork();
                 if (pid == 0) {
+                    clock_t t = clock();
                     licznik++;
-                    _exit(0);
+                    t = clock() - t;
+                    _exit(t);
                 } else {
                     int status;
                     wait(&status);
+                    childtimes += WEXITSTATUS(status);
                 }
                 break;
             }
             case 'b': {
                 pid = vfork();
                 if (pid == 0) {
+                    clock_t t = clock();
                     licznik++;
-                    _exit(licznik);
+                    t = clock() - t;
+                    _exit(t);
                 } else {
                     int status;
                     wait(&status);
+                    childtimes += WEXITSTATUS(status);
                 }
                 break;
             }
             case 'c': {
-                for (int j = 0; j < n; j++) {
-                    clone(&fn, child_stack, SIGCHLD, NULL);
-                    int status;
-                    wait(&status);
-                }
+                clone(&fn, child_stack, SIGCHLD, NULL);
+                int status;
+                wait(&status);
+                childtimes += WEXITSTATUS(status);
                 break;
             }
             case 'd': {
                 clone(&fn, child_stack, SIGCHLD | CLONE_VM | CLONE_VFORK, NULL);
                 int status;
                 wait(&status);
+                childtimes += WEXITSTATUS(status);
             }
                 break;
             default:
                 break;
         }
     }
+    clock_t endtime = clock() - starttime;
+    struct rusage *rus = malloc(sizeof(struct rusage));
+    getrusage(RUSAGE_SELF, rus);
+    printf("System Time: %li, User Time: %li, User+System Time: %li, Real Time: %li\n",
+           rus->ru_stime.tv_sec, rus->ru_utime.tv_sec, rus->ru_utime.tv_sec + rus->ru_stime.tv_sec,
+           endtime / CLOCKS_PER_SEC);
+    printf("\n");
+    struct rusage *rus1 = malloc(sizeof(struct rusage));
+    getrusage(RUSAGE_CHILDREN, rus1);
+    printf("System Time: %li, User Time: %li, User+System Time: %li, Real Time: %li\n",
+           rus1->ru_stime.tv_sec, rus1->ru_utime.tv_sec,
+           rus1->ru_utime.tv_sec + rus1->ru_stime.tv_sec, childtimes / CLOCKS_PER_SEC);
+    printf("\n");
 
-    t = times(t_cpu2) - t;
-    printf("Real Time: %jd, User Time: %jd, System Time: %jd\n", (intmax_t) t,
-           (intmax_t) (t_cpu2->tms_utime - t_cpu1->tms_utime),
-           (intmax_t) (t_cpu2->tms_stime - t_cpu1->tms_stime));
-    printf("licznik: %d\n", licznik);
+    printf("System Time: %li, User Time: %li, User+System Time: %li, Real Time: %li\n",
+           rus1->ru_stime.tv_sec + rus->ru_stime.tv_sec, rus->ru_utime.tv_sec + rus1->ru_utime.tv_sec,
+           rus->ru_utime.tv_sec + rus->ru_stime.tv_sec + rus1->ru_utime.tv_sec + rus1->ru_stime.tv_sec,
+           (endtime + childtimes) / CLOCKS_PER_SEC);
 
-    free(t_cpu1);
-    free(t_cpu2);
     return 0;
 }
 
